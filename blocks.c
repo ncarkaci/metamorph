6e2a6108e348c9bef7e7e1d9c3c38beb5bff9020
quetuo <quetuo@310d7af5-19c2-4b9c-8775-fcb2e12e4a67>
2011-02-21 01:51:21 +0000

    Block padding

--- blocks.c
+++ blocks.c
@@ -26,11 +26,29 @@ classBlock::~classBlock () {
 classBlockContainer::classBlockContainer (std::vector <std::string> _lines) {
 	blockSize = getBlockSize ();
 	int offset = 0;
+	classBlock _block;
+	int block = 0;
+	int blocks = 0;
 	for (int i = 0; i < (_lines.size () - 1); i ++) {
 		std::string address = _lines [i].substr (0, 8);
 		std::string op = _lines [i].substr (28);
-		printDebug ("BLK", "At 0x" + address + ": " + op);
-		
+		std::string _size = _lines [i].substr (10, 18);
+		size_t pos = _size.find (" ");
+		int size = pos / 2;
+		if (offset % blockSize == 0) {
+			blocks += 1;
+			printDebug ("BLK", "New block (" + itoa (blocks) + ")");
+		}
+		printDebug ("BLK", itoa (offset) + ": At 0x" + address + ": " + op + "(" + itoa(size) + " bytes)");
+		if ((offset + size) - ((blocks - 1) * blockSize) > blockSize) {
+			int _padding = blockSize - (offset % blockSize);
+			std::vector <std::string>::iterator it = _lines.begin();
+			std::advance (it, i);
+			_lines.insert (it, _padding, "NOP");
+			printDebug ("BLK", "Padding (" + itoa (_padding) + ")...");
+			offset += _padding;
+		}
+		else {offset += size;}
 	}
 }
 
